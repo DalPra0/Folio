ed156bd0cc94faba4fda66b3f15663d966db5cb9
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-09-29 19:52:02 -0300

    Initialize Nano03FolioProject iOS app
    Add initial Xcode project files for Nano03FolioProject, including AppDelegate, SceneDelegate, ViewController, asset catalogs, storyboards, Info.plist, entitlements, and Core Data model. Sets up a basic UIKit app structure with Core Data and CloudKit support.

---
+++ Nano03FolioProject/Nano03FolioProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano03FolioProject
+//
+//  Created by Lucas Dal Pra Brascher on 29/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
